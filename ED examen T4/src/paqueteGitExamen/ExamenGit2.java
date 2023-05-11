c8fecd3229aa09d30c9eb3e5d603d300315c208b
Alejandro Chenoll <achg7396@gmail.com>
2023-05-11 20:12:42 +0200

    Segunda version de Git

---
+++ ED examen T4/src/paqueteGitExamen/ExamenGit2.java
@@ -0,0 +1,12 @@
+package paqueteGitExamen;
+
+public class ExamenGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sigo haciendo el examen :D");
+		
+		System.out.println("Estoy terminando el examen :D");
+	}
+
+}
